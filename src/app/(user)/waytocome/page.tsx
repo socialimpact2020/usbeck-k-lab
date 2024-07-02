e75819da1235d0dc1bf1df9570eb4ad50b74c37d
elcodeofficial-kr <admin@elcode.co.kr>
2024-07-02 18:11:06 +0900

    update map

--- src/app/(user)/waytocome/page.tsx
+++ src/app/(user)/waytocome/page.tsx
@@ -41,7 +41,7 @@ export default function WaytoCome() {
 
         <div className="mb-10">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2997.1518968960327!2d69.27849151203073!3d41.30555907119057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8ad671d84dcb%3A0x3175fb9f24cd3354!2zMjUgU2hha2hyaXNhYnogU3RyZWV0LCBUYXNoa2VudCwg7Jqw7KaI67Kg7YKk7Iqk7YOE!5e0!3m2!1sko!2skr!4v1703168927173!5m2!1sko!2skr"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5994.381531241549!2d69.28084009302125!3d41.30471344103923!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8ad671d84dcb%3A0x3175fb9f24cd3354!2sShakhrisabz%20Street%2025%2C%20Tashkent%2C%20Uzbekistan!5e0!3m2!1sen!2skr!4v1719911360020!5m2!1sen!2skr"
             width="100%"
             height="700"
             loading="lazy"
