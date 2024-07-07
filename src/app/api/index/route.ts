2159ad543e1c2b32f4453e3e3f6968700f8036f2
elcodeofficial-kr <admin@elcode.co.kr>
2024-07-07 11:47:43 +0900

    promise.all

--- src/app/api/index/route.ts
+++ src/app/api/index/route.ts
@@ -1,42 +1,50 @@
 import client from "@/libs/server/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET() {
   try {
-    const courses = await client.course.findMany({
-      orderBy: { createdAt: "desc" },
-      take: 4,
-      include: { progressPeriod: true, recruitmentPeriod: true },
-    });
-    const notices = await client.post.findMany({
-      where: { type: "Notice" },
-      orderBy: { createdAt: "desc" },
-      select: {
-        title: true,
-        id: true,
-        type: true,
-        createdAt: true,
-      },
-      take: 5,
-    });
-    const news = await client.post.findMany({
-      where: { type: "News" },
-      orderBy: { createdAt: "desc" },
-      select: {
-        title: true,
-        id: true,
-        type: true,
-        createdAt: true,
-      },
-      take: 5,
-    });
+    console.log("Fetching data...");
+
+    const [courses, notices, news] = await Promise.all([
+      client.course.findMany({
+        orderBy: { createdAt: "desc" },
+        take: 4,
+        include: { progressPeriod: true, recruitmentPeriod: true },
+      }),
+      client.post.findMany({
+        where: { type: "Notice" },
+        orderBy: { createdAt: "desc" },
+        select: {
+          title: true,
+          id: true,
+          type: true,
+          createdAt: true,
+        },
+        take: 5,
+      }),
+      client.post.findMany({
+        where: { type: "News" },
+        orderBy: { createdAt: "desc" },
+        select: {
+          title: true,
+          id: true,
+          type: true,
+          createdAt: true,
+        },
+        take: 5,
+      }),
+    ]);
+
+    console.log("Courses fetched:", courses);
+    console.log("Notices fetched:", notices);
+    console.log("News fetched:", news);
 
     const response = NextResponse.json({ ok: true, courses, notices, news });
-    response.headers.set("Cache-Control", "public, max-age=0, must-revalidate"); // 캐싱 최소화
+    response.headers.set("Cache-Control", "public, max-age=0, must-revalidate");
     return response;
   } catch (err) {
+    console.error("Error fetching data:", err);
     return NextResponse.json({ ok: false, message: err });
   }
 }
